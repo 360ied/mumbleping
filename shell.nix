d8ff7f6b322b7b993101ceff811bf525acd61306
360ied <19516527+360ied@users.noreply.github.com>
2022-05-22 18:32:49 -0400

    add gotools to shell.nix

--- shell.nix
+++ shell.nix
@@ -4,4 +4,4 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.11.tar.gz";
     sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
   }) { };
-in pkgs.mkShell { nativeBuildInputs = [ pkgs.go_1_17 ]; }
+in pkgs.mkShell { nativeBuildInputs = [ pkgs.go_1_17 pkgs.gotools ]; }
